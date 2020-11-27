61145c7aa55d1ee8142977f95b7f3e306a7528d2
unknown <haseebhowto@gmail.com>
2020-11-27 10:32:12 +0500

    Input Field added

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function App() {
+  const [orgPrice, setOrgPrice] = useState(0);
+  const [disPercent, setDisPercent] = useState(0);
+
+  const orgPriceHandler = (price) => {
+    setOrgPrice(price);
+  };
+
+  const disPercentHandler = (percent) => {
+    setDisPercent(percent);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <View>
+        <Text>Original Price:</Text>
+        <TextInput
+          style={styles.inputFields}
+          onChangeText={(price) => orgPriceHandler(price)}
+        />
+
+        <Text>Discount Percentage:</Text>
+        <TextInput
+          style={styles.inputFields}
+          onChangeText={(price) => disPercentHandler(price)}
+        />
+      </View>
     </View>
   );
 }
@@ -14,8 +38,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputFields: {
+    height: 40,
+    width: 200,
+    padding: 10,
+    borderWidth: 1,
+    marginBottom: 30,
   },
 });
