e6a27de2a28212a076ec9384284c306d861e2db8
unknown <haseebhowto@gmail.com>
2020-11-27 21:04:10 +0500

    added additional features

--- App.js
+++ App.js
@@ -1,49 +1,100 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from 'react-native';
 
 export default function App() {
-  const [orgPrice, setOrgPrice] = useState(0);
-  const [disPercent, setDisPercent] = useState(0);
+  const [orgPrice, setOrgPrice] = useState('');
+  const [disPercent, setDisPercent] = useState('');
   // const [final, setFinal] = useState({});
 
   const orgPriceHandler = (price) => {
-    setOrgPrice(price);
+    if (price >= 0) {
+      setOrgPrice(price);
+    }
   };
 
   const disPercentHandler = (percent) => {
-    setDisPercent(percent);
+    if (percent <= 100) {
+      setDisPercent(percent);
+    }
   };
 
   const saveAmount = () => {
-    let disAmount = orgPrice * ( disPercent / 100 )
-    return disAmount;
-  }
+    let disAmount = orgPrice * (disPercent / 100);
+    return disAmount.toFixed(2);
+  };
   const finalPrice = () => {
-    let finalAmount = orgPrice - saveAmount()
-     return finalAmount;
+    let finalAmount = orgPrice - saveAmount();
+    return finalAmount.toFixed(2);
+  };
+
+  const clearInputData = () => {
+    setOrgPrice('');
+    setDisPercent('');
   }
 
   return (
     <View style={styles.container}>
       <View>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            margin: 40,
+          }}>
+          <TouchableOpacity>
+            <Text>STORED RECORDS</Text>
+          </TouchableOpacity>
+        </View>
+
         <Text>Original Price:</Text>
         <TextInput
           style={styles.inputFields}
-          onChangeText={(price) => orgPriceHandler(price)}
+          keyboardType="numeric"
+          onChangeText={orgPriceHandler}
+          value={orgPrice}
         />
         <Text>Discount Percentage:</Text>
         <TextInput
           style={styles.inputFields}
-          onChangeText={(price) => disPercentHandler(price)}
+          keyboardType="numeric"
+          onChangeText={disPercentHandler}
+          value={disPercent}
         />
 
-        <Text>
-          {(orgPrice !== 0 && disPercent !== 0 ) ? "You Save: "+saveAmount() : ""}
-        </Text>
-        <Text>
-          {(orgPrice !== 0 && disPercent !== 0 ) ? "Final Price: "+finalPrice() : ""}
-        </Text>
+        <View style={{ textAlign: 'center' }}>
+          <Text>
+            {orgPrice !== 0 && disPercent !== 0
+              ? 'You Save : $' + saveAmount()
+              : ''}
+          </Text>
+          <Text>
+            {orgPrice !== 0 && disPercent !== 0
+              ? 'Final Price : $' + finalPrice()
+              : ''}
+          </Text>
+        </View>
 
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            margin: 20,
+          }}>
+          <TouchableOpacity style={styles.btnSave}>
+            <Text>SAVE</Text>
+          </TouchableOpacity>
+          
+          <TouchableOpacity style={styles.btnClear} onPress={() => clearInputData()}>
+            <Text>CLEAR</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
@@ -52,9 +103,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   inputFields: {
     height: 40,
@@ -62,5 +113,19 @@ const styles = StyleSheet.create({
     padding: 10,
     borderWidth: 1,
     marginBottom: 30,
+  },
+  btnSave: {
+    width: 80,
+    height: 40,
+    backgroundColor: '#FF3B11',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  btnClear: {
+    width: 80,
+    height: 40,
+    backgroundColor: 'lightgrey',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
